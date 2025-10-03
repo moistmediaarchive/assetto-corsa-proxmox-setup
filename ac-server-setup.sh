d220ff2090977773aecd6bd30e62cab983fd7a05
moistmediaarchive <moistmediaarchive@gmail.com>
2025-10-03 05:27:34 -0700

    after working

--- ac-server-setup.sh
+++ ac-server-setup.sh
@@ -30,7 +30,7 @@ echo -e " / /  / / /_/ // / ___/ // /_____/ ___ / /___   "
 echo -e "/_/  /_/\____/___//____//_/     /_/  |_\____/   ${RESET}"
 echo
 echo
-echo -e "${YELLOW}Moist AC Server and Discord Bot Auto Setup${RESET} - version 1.13"
+echo -e "${YELLOW}Moist AC Server and Discord Bot Auto Setup${RESET} - version 1.14"
 echo -e "${YELLOW}Read the documentation if you need help: <link placeholder>"
 echo
 echo
@@ -104,16 +104,10 @@ done
 
 echo -e "${GREEN}[+] LXC Started.${RESET}"
 
-
-
-
-# echo -e "${BLUE}[>] Updating container - this may take some time ...${RESET}"
-# pct exec $CTID -- bash -c "apt-get -qq update && apt-get -qq -y upgrade" >/dev/null 2>&1 &
-# spinner $!
-# echo -e "${GREEN}[+] LXC Updated.${RESET}"
-
-
-
+echo -e "${BLUE}[>] Updating container - this may take some time ...${RESET}"
+pct exec $CTID -- bash -c "apt-get -qq update && apt-get -qq -y upgrade" >/dev/null 2>&1 &
+spinner $!
+echo -e "${GREEN}[+] LXC Updated.${RESET}"
 
 echo -e "${BLUE}[>] Installing dependencies - this may take some time ...${RESET}"
 pct exec $CTID -- bash -c "apt-get -qq install -y unzip python3-venv python3-pip git ufw" >/dev/null 2>&1 &
@@ -439,7 +433,7 @@ pct exec $CTID -- bash -c "
             echo -e '${GREEN}[+] Initial setup complete for: ${RESET}' \$(basename \"\$track_dir\")
         fi
     done
-"
+" >/dev/null 2>&1 &
 
 pct exec $CTID -- bash -c "
     for track_dir in /home/$USERNAME/assetto-servers/*/; do
@@ -448,6 +442,8 @@ pct exec $CTID -- bash -c "
         chmod -R u+rw \"\$track_dir/cfg\"
     done
 "
+
+spinner $!
 
 echo -e "${GREEN}[+] All track servers have completed their initial setup.${RESET}"
 
@@ -525,7 +521,7 @@ for track_dir in /home/\$USERNAME/assetto-servers/*/; do
                 fi
 
                 if [ -f \"\$entry_list\" ]; then
-                    echo \"[>] Updating \$entry_list for AI traffic...\"
+
                     sed -i '/^AI=/d' \"\$entry_list\"
 
                     tmpfile=\$(mktemp)
@@ -540,7 +536,7 @@ for track_dir in /home/\$USERNAME/assetto-servers/*/; do
                         fi
                     done < \"\$entry_list\"
                     mv \"\$tmpfile\" \"\$entry_list\"
-                    echo \"[+] AI traffic injected into \$entry_list\"
+
                 else
                     echo \"[!] entry_list.ini not found for \$track_name\"
                 fi
@@ -553,7 +549,7 @@ for track_dir in /home/\$USERNAME/assetto-servers/*/; do
     # --- Append INFINITE=1 ---
     if [ -f \"\$server_cfg\" ]; then
         echo \"INFINITE=1\" >> \"\$server_cfg\"
-        echo \"[+] Added INFINITE=1 to server_cfg.ini\"
+
     fi
 
     # --- Move fast_lane.aip if present ---
@@ -563,7 +559,7 @@ for track_dir in /home/\$USERNAME/assetto-servers/*/; do
             dest=\"\$inner_track_dir/ai\"
             mkdir -p \"\$dest\"
             mv \"\$track_dir/fast_lane.aip\" \"\$dest/\"
-            echo \"[+] Moved fast_lane.aip into \$dest\"
+
         else
             echo \"[!] No track folder found inside \$track_dir/content/tracks, skipping fast_lane.aip move.\"
         fi
@@ -579,5 +575,15 @@ pct exec $CTID -- bash $CONFIG_SCRIPT
 # Fix permissions after configuration
 pct exec $CTID -- chown -R $USERNAME:$USERNAME /home/$USERNAME/assetto-servers
 
-echo -e "${RED}COMPLETE${RESET}"
+echo
+echo
+echo
+
+echo -e "${GREEN} +++ SERVER SETUP COMPLETE +++ ${RESET}"
+echo -e "${YELLOW}Remember to port forward ports 9600/tcp/udp and 8081/tcp on your router${RESET}"
+echo -e "${YELLOW}The server will not work without port forwarding.${RESET}"
+
+echo
+echo -e "${GREEN} +++ Happy Racing! +++ ${RESET}"
+
 
